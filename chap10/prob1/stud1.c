57ffef9b05f570afe6ce156454cc41b9b78bd241
userdok <04kkukkum@gmail.com>
2024-11-18 01:22:53 +0000

    .

---
+++ chap10/prob1/stud1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct student {
+	int id;
+	char name[20];
+};
+
+int main() {
+	struct student *ptr;
+	int n, i;
+
+	printf("How many students do you want to enter?");
+	scanf("%d", &n);
+	if (n <= 0) {
+		fprintf(stderr, "Error: Invalid number of students.\n");
+		fprintf(stderr, "End the program.");
+		exit(1);
+	}
+
+	ptr = (struct student *)malloc(n * sizeof(struct student));
+	if (ptr == NULL) {
+		perror("malloc");
+		exit(2);
+	}
+
+	printf("%d Please enter your student number and name.\n", n);
+	for (i = 0; i < n; i++)
+		scanf("%d %s", &ptr[i].id, ptr[i].name);
+
+	printf("\n * Student Information (reverse order) *\n");
+	for (i = n-1; i >= 0; i--)
+		printf("%d %s\n", ptr[i].id, ptr[i].name);
+
+	printf("\n");
+	exit(0);
+}
